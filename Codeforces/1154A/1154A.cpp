7a10531af913dad0094fcc77cd1e7b4cf9c9bec5
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-06-03 17:35:23 +0600

    A. storing three numbers

---
+++ Codeforces/1154A/1154A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int i=0;
+  int numbers[4];
+  for(i=0; i<4; i++) {
+    cin>>numbers[i];
+  }
+  sort(numbers, numbers+4);
+  cout<<numbers[3]-numbers[0]<<" ";
+  cout<<numbers[3]-numbers[1]<<" ";
+  cout<<numbers[3]-numbers[2]<<endl;
+  return 0;
+}
