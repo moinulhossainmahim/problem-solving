0ce6ec40c8b3cd8a4b2fc9a4ccaebc656e5833b8
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-06-08 22:04:17 +0600

    A. Vasya and coins

--- Codeforces/1660A/1660A.cpp
+++ Codeforces/1660A/1660A.cpp
@@ -9,14 +9,7 @@ int main() {
   while(t--) {
     cin>>a>>b;
     if(a==0) cout<<1<<endl;
-    else if(b==0) {
-      n1=a*1;
-      cout<<n1+1<<endl;
-    } else {
-      n1=a*1;
-      n2=b*2;
-      cout<<n1+n2+1<<endl;
-    }
+    else cout<<(2*b+a+1)<<endl;
   }
   return 0;
 }
