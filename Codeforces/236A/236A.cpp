3b9e866f00fd76e0815332ad5237e42ccf7dba81
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-06-09 22:36:23 +0600

    A. Boy or girl

--- Codeforces/236A/236A.cpp
+++ Codeforces/236A/236A.cpp
@@ -1,1 +1,13 @@
 
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string s1;
+  cin>>s1;
+  sort(s1.begin(), s1.end());
+  s1.erase(unique(s1.begin(), s1.end()), s1.end());
+  s1.length() & 1?cout<<"IGNORE HIM!"<<endl:cout<<"CHAT WITH HER!"<<endl;
+  return 0;
+}
