d7f9f8e9ec86dab6305fa5a766c885fba88008b1
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-08-02 22:19:29 +0600

    A Round down the price

---
+++ Codeforces/1634A/1634A.cpp
@@ -0,0 +1,63 @@
+/*
+  IN THE NAME OF ALLAH
+  Author: Moinul Hossain
+  Northern University Bangladesh
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 1000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+ll gcd ( ll a, ll b ) { return __gcd ( a, b ); }
+ll lcm ( ll a, ll b ) { return a * ( b / gcd ( a, b ) ); }
+
+int dx[] = { 0, 0, +1, -1, -1  +1, -1, +1 };
+int dy[] = { +1, -1, 0, 0, -1, +1, +1, -1 };
+
+
+int main() {
+  optimize();
+  int t;
+  cin >> t;
+  while(t--) {
+    int n, m;
+    cin >> n >> m;
+    string s,temp;
+    cin >> s;
+    temp=s;
+    reverse(temp.begin(), temp.end());
+    if(s==temp) cout << 1 << endl;
+    else if(m>=1) cout << 2 << endl;
+    else cout << 1 << endl;
+  }
+}
