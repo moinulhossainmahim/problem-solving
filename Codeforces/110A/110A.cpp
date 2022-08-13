99f184d5187f1e718d48583601fd3bc25c06d070
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-08-13 16:58:09 +0600

    Added

--- Codeforces/110A/110A.cpp
+++ Codeforces/110A/110A.cpp
@@ -1,21 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
   long long n;
-  int count=0;
-  cin>>n;
-  while(n) {
-    if(n%10 == 4 || n%10 == 7) {
+  int count = 0;
+  cin >> n;
+  while (n) {
+    if (n % 10 == 4 || n % 10 == 7) {
       count++;
     }
-    n = n/10;
+    n = n / 10;
   }
-  if(count == 4 || count == 7) {
-  cout<<"YES"<<endl;
-  }
-  else {
-  cout<<"NO"<<endl;
+  if (count == 4 || count == 7) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
   }
   return 0;
 }
--- Codeforces/1567A/1567A.cpp
+++ Codeforces/1567A/1567A.cpp
@@ -1,25 +1,25 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-  int s,n,i;
+  int s, n, i;
   string s1;
-  cin>>s;
-  while(s--) {
+  cin >> s;
+  while (s--) {
     string s2;
-    cin>>n;
-    cin>>s1;
-    for(i=0; i<n; i++) {
-      if(s1[i]=='U') {
-        s2+='D';
-      } else if(s1[i]=='D'){
-        s2+='U';
-      } else if(s1[i]=='L' || s1[i]=='R') {
-        s2+=s1[i];
+    cin >> n;
+    cin >> s1;
+    for (i = 0; i < n; i++) {
+      if (s1[i] == 'U') {
+        s2 += 'D';
+      } else if (s1[i] == 'D') {
+        s2 += 'U';
+      } else if (s1[i] == 'L' || s1[i] == 'R') {
+        s2 += s1[i];
       }
     }
-    cout<<s2<<endl;
+    cout << s2 << endl;
   }
   return 0;
 }
