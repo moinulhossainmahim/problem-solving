356b3632bdc69b7dbf21f4c8bae3b0023d34d090
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-08-21 02:13:05 +0600

    A. Equidistant Letters

--- Codeforces/1626A/1626A.cpp
+++ Codeforces/1626A/1626A.cpp
@@ -1,1 +1,29 @@
+/*
+  IN THE NAME OF ALLAH
+  Author: Moinul Hossain
+  Northern University Bangladesh
+*/
 
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+#define debug(n) cout << (n) << endl;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    cout << s << endl;
+  }
+}
