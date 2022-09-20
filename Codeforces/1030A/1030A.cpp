29a4afa784ddc33d9b175334a0cf5f44773b9d6d
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-09-20 23:12:17 +0600

    Added cf problem soln

--- Codeforces/1030A/1030A.cpp
+++ Codeforces/1030A/1030A.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main() {
   int n;
-  bool isEasy=true;
-  cin>>n;
-  for(int i=0; i<n; i++) {
+  bool isEasy = true;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
     int a;
-    cin>> a;
-    if(a == 1) {
+    cin >> a;
+    if (a == 1) {
       isEasy = false;
     }
   }
-  if(isEasy) {
-    cout<<"EASY"<<endl;
+  if (isEasy) {
+    cout << "EASY" << endl;
   } else {
-    cout<<"HARD"<<endl;
+    cout << "HARD" << endl;
   }
   return 0;
 }
---
+++ Codeforces/1700A/1700A.cpp
@@ -0,0 +1,33 @@
+/*
+  IN THE NAME OF ALLAH
+  Author: Moinul Hossain
+  Northern University Bangladesh
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+#define debug(n) cout << (n) << endl;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int n, m;
+    cin >> n >> m;
+    ll sum = 0;
+    for (int i = 1; i <= m; i++)
+      sum += i;
+    for (int i = 2; i <= n; i++)
+      sum += (i * m);
+    cout << sum << '\n';
+  }
+}
